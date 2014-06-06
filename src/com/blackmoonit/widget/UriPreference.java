ebfe788eb4ea7ec6bedfe30106c10b86fa4c179e
baracudda <ryan.fischbach@istresearch.com>
2014-06-06 17:15:13 -0400

    SliderPreference added, modified UriPreference to descend from StringPreference (looks/works much better now).

--- src/com/blackmoonit/widget/UriPreference.java
+++ src/com/blackmoonit/widget/UriPreference.java
@@ -3,38 +3,19 @@ package com.blackmoonit.widget;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 
-public class UriPreference extends TextDialogPreference {
+public class UriPreference extends StringPreference {
 
 	public UriPreference(Context aContext, AttributeSet attrs) {
 		super(aContext, attrs);
 	}
 
 	@Override
-	protected EditText createEditText(Context aContext) {
-		super.createEditText(aContext);
-		mEditText.setInputType(EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_VARIATION_URI);
-		return mEditText;
+	protected void onAddEditTextToDialogView(View aDialogView, EditText aEditText) {
+		super.onAddEditTextToDialogView(aDialogView, aEditText);
+		aEditText.setInputType(EditorInfo.TYPE_CLASS_TEXT | EditorInfo.TYPE_TEXT_VARIATION_URI);
 	}
 	
-	@Override
-	protected View onCreateView(ViewGroup parent) {
-		setSummary(getEntry());
-		return super.onCreateView(parent);
-	}
-
-	@Override
-	protected boolean persistString(String aNewValue) {
-		if (super.persistString(aNewValue)) {
-			setSummary(getEntry());
-			notifyChanged();
-			return true;
-		} else {
-			return false;
-		}
-	}
-
 }
