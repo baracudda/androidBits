4781bc1627faf7a1e21f944055ade834096dd49d
Ryan Fischbach <ryan.fischbach@istresearch.com>
2015-07-13 13:20:21 -0400

    ProviderService ancestor class: mDb may not be defined yet, so force descendant to provide the "static result".

--- lib_androidBits/src/main/java/com/blackmoonit/androidbits/database/ProviderService.java
+++ lib_androidBits/src/main/java/com/blackmoonit/androidbits/database/ProviderService.java
@@ -4,9 +4,11 @@ import com.blackmoonit.androidbits.content.SqlContractProvider;
 
 abstract public class ProviderService extends SqlContractProvider {
 
+	/* mDb may not be defined yet, so force descendant to provide the static result.
 	@Override
 	protected ProviderContract.Database getDbContract() {
 		return ((ProviderDatabase)mDb).getDbContract();
 	}
+	*/
 
 }
